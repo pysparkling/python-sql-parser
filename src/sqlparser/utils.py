7cbb7d038da2b9e6c3644b95daed1b2017679c63
tools4origins <tools4origins@gmail.com>
2020-10-24 22:55:34 +0200

    Make debug utils compatible with parser API

--- src/sqlparser/utils.py
+++ src/sqlparser/utils.py
@@ -1,14 +1,12 @@
-def print_tree(tree):
-    print(type(tree).__name__)
-    print_sub_tree(tree, indent=0)
+def print_tree(tree, printer=print):
+    for line in tree_to_strings(tree, indent=0):
+        printer(line)
 
 
-def print_sub_tree(tree, indent=0):
-    for c in tree.children:
-        print(
-            "|" + "-" * indent,
-            type(c).__name__,
-            ("[" + c.symbol.text + "]") if hasattr(c, "symbol") else ""
-        )
-        if hasattr(c, 'children') and c.children:
-            print_sub_tree(c, indent + 2)
+def tree_to_strings(tree, indent=0):
+    node_as_string = type(tree).__name__ + (("[" + tree.symbol.text + "]") if hasattr(tree, "symbol") else "")
+    result = ["|" + "-" * indent + node_as_string]
+    if hasattr(tree, 'children') and tree.children:
+        for c in tree.children:
+            result += tree_to_strings(c, indent + 2)
+    return result
