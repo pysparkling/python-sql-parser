aa18978a708a3cf9449e701c8fd9ed90e0274eb7
tools4origins <tools4origins@gmail.com>
2020-10-24 22:54:52 +0200

    Expose more methods

--- src/sqlparser/__init__.py
+++ src/sqlparser/__init__.py
@@ -1,1 +1,10 @@
+from .internalparser import SqlSyntaxError
 from .stringparser import string_to_ast
+from .parsing import parse_sql, \
+    parse_statement, \
+    parse_expression, \
+    parse_table_identifier, \
+    parse_multipart_identifier, \
+    parse_function_identifier, \
+    parse_data_type, \
+    parse_table_schema
