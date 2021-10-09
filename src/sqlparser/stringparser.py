f621393f5d9f9931fe6a0d05b321b834315ca509
tools4origins <tools4origins@gmail.com>
2021-10-09 14:39:24 +0200

    Expose bail strategy choice in package entrypoint

--- src/sqlparser/stringparser.py
+++ src/sqlparser/stringparser.py
@@ -6,17 +6,17 @@ from sqlparser.internalparser import build_parser
 from sqlparser.utils import print_tree
 
 
-def string_to_ast(string, rule, *, strict_mode=False, debug=False):
-    parser = build_string_parser(string, strict_mode)
+def string_to_ast(string, rule, *, strict_mode=False, debug=False, early_bail=False):
+    parser = build_string_parser(string, strict_mode, early_bail)
     tree = getattr(parser, rule)()
     if debug:
         print_tree(tree, printer=logging.warning)
     return tree
 
 
-def build_string_parser(string, strict_mode=False):
+def build_string_parser(string, strict_mode=False, early_bail=True):
     string_as_stream = InputStream(string)
-    parser = build_parser(string_as_stream, strict_mode)
+    parser = build_parser(string_as_stream, strict_mode, early_bail)
     return parser
 
 
